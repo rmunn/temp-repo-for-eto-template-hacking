577fd768521416c950bd04fbfb5143b8531ca370
Robin Munn <rmunn@pobox.com>
2015-10-27 23:25:50 +0700

    Snippet: panel from Json

---
+++ ApplicationName/ApplicationName/PanelFromJson.jeto.fs
@@ -0,0 +1,12 @@
+﻿namespace ApplicationName
+
+open System
+open Eto.Forms
+open Eto.Drawing
+open Eto.Serialization.Json
+
+type PanelFromJson () as this =
+    inherit Panel ()
+
+    do
+        JsonReader.Load(this, "PanelFromJson.jeto")
