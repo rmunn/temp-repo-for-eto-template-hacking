55e69e5b90b6ae0aaa2620e3165397d326c3589e
Robin Munn <rmunn@pobox.com>
2015-10-27 23:28:53 +0700

    Drawable from Xaml instead of Scrollable

--- ApplicationName/ApplicationName/PanelFromXaml.xeto.fs
+++ ApplicationName/ApplicationName/PanelFromXaml.xeto.fs
@@ -6,7 +6,11 @@ open Eto.Drawing
 open Eto.Serialization.Xaml
 
 type PanelFromXaml () as this =
-    inherit Scrollable ()
+    inherit Drawable ()
 
     do
         XamlReader.Load(this, "PanelFromXaml.xeto")
+
+    override base.OnPaint(e: PaintEventArgs) =
+        // your custom drawing
+        e.Graphics.FillRectangle(Colors.Blue, e.ClipRectangle)
